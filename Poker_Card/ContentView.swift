bafd14b6d8c7a104415e7dc4ff9203b5a6c89470
Jim <chenjim821ddt@gmail.com>
2021-03-10 22:42:22 +0800

    Initial Commit

---
+++ Poker_Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Poker_Card
+//
+//  Created by 陳元浚 on 2021/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Poker_Card/Poker_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_CardApp.swift
+//  Poker_Card
+//
+//  Created by 陳元浚 on 2021/3/10.
+//
+
+import SwiftUI
+
+@main
+struct Poker_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
